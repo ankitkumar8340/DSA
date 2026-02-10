068ab88d914758a97dee5af60a639e40857a0977
ankit <ankit@example.com>
2026-02-10 22:54:42 +0530

    updated

---
+++ Array/AK/insertion1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+ //insertion at beg
+
+ void insertbeg(int arr[], int size){
+    // size++;
+    for(int i=size-1;i>=0;i--){
+        arr[i+1]=arr[i];
+    }
+
+
+
+    size++;
+    arr[0]=10;
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+ }
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+   
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i];
+    // }
+    insertbeg(arr, n);
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
