7f2903250891c59f3819d1cd678421a57633f316
ankit <ankit@example.com>
2026-02-10 23:50:03 +0530

    updated

--- Array/AK/insertion1.cpp
+++ Array/AK/insertion1.cpp
@@ -40,6 +40,40 @@ using namespace std;
     }
  }
 
+ void bubbleSort(int arr[], int size){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+ }
+
+ void selectionSort(int arr[], int size){
+    for(int i=0;i<size-1;i++){
+        int minindex= i;
+        for(int j=i+1;j<size;j++){
+            if(arr[minindex] > arr[j]){
+                minindex=j;
+            }
+        }
+        int temp = arr[i];
+        arr[i]=arr[minindex];
+        arr[minindex]=temp;
+    }
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+ }
+
 int main(){
     int n;
     cin>>n;
@@ -57,10 +91,19 @@ int main(){
 
     // insertAtAny(arr, n);
 
-    
+    //sorting 
+
+    // bubbleSort(arr, n);
+
+    selectionSort(arr, n);
+
+    // insertionSort(arr, n);
+
+
+
     
 
-    // mera bhi hogya mai ati washroom jari vc cut krti tbtk
+    
     
 }
 
