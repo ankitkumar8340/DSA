718c4f78b351cef3b380dc3afa04144f6411677f
ankit <ankit@example.com>
2026-02-11 00:11:40 +0530

    updated

--- Array/AK/insertion1.cpp
+++ Array/AK/insertion1.cpp
@@ -74,6 +74,24 @@ using namespace std;
     }
  }
 
+ void insertionSort(int arr[], int n){
+    for(int i=1;i<n;i++){
+        int key = arr[i];
+        int j= i-1;
+        while(j>=0 && arr[j]> key ){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+ }
+
+
+
 int main(){
     int n;
     cin>>n;
@@ -95,9 +113,11 @@ int main(){
 
     // bubbleSort(arr, n);
 
-    selectionSort(arr, n);
+    // selectionSort(arr, n);
 
-    // insertionSort(arr, n);
+    insertionSort(arr, n);
+
+
 
 
 
