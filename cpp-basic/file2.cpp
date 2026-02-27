3bbfb38bff880b406363c58d20bf75d648c97d52
ankit <ankitkumarswe01@gmail.com>
2026-02-27 17:06:02 +0530

    updated

--- cpp-basic/file2.cpp
+++ cpp-basic/file2.cpp
@@ -11,6 +11,20 @@ int main(){
     //     cout<<arr[i]<<" ";
     // }
 
+    int arr[2][3];
+    
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
 
     return 0;
