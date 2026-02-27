15b425af11c7792a006b93029c65a0b59e960aa2
ankit <ankitkumarswe01@gmail.com>
2026-02-27 16:41:06 +0530

    updated

--- cpp-basic/file1.cpp
+++ cpp-basic/file1.cpp
@@ -5,12 +5,58 @@ using namespace std;
 
 int main(){
     // cout<<"Hey ankit"<<endl;
-    int x,y;
-    cin>>x>>y;
-    cout<<x<<" "<<y;
+    // int x,y;
+    // cin>>x>>y;
+    // cout<<x<<" "<<y;
 
-    float x = 4.6;
-    float y = 3;
+    // float x = 4.6;
+    // float y = 3;
+
+    // string s1;
+    // cin>>s1;
+    // cout<<s1;
+
+
+    // string str;
+    // getline(cin, str);
+    // cout<<str;
+
+    // char ch = 'g';
+    // // cin>>ch;
+    // cout<<ch;
+
+ /*//if - else
+    int age;
+    cin>>age;
+    if(age>=18){
+        cout<<"Adult";
+    }
+    else{
+        cout<<"Not Adult";
+    }
+        */   
+
+        int marks;
+        cin>>marks;
+
+        if(marks<24){
+            cout<<"F";
+        }
+        else if( marks < 44){
+            cout<<"E";
+        }
+        else if(marks < 64){
+            cout<<"D";
+        }
+        else if(marks < 74){
+            cout<<"C";
+        }
+        else if(marks < 90){
+            cout<<"B";
+        }
+        else if(marks <= 100){
+            cout<<"A"; 
+        }
 
 
 
@@ -18,7 +64,10 @@ int main(){
 
 
 
-    
+
+
+
+
     return 0;            
 
 }
