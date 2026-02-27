ec77d8bb2b02b190fc558d1b85558c57b9f0662c
ankit <ankitkumarswe01@gmail.com>
2026-02-27 16:49:27 +0530

    updated

--- cpp-basic/file1.cpp
+++ cpp-basic/file1.cpp
@@ -36,26 +36,55 @@ int main(){
     }
         */   
 
-        int marks;
-        cin>>marks;
+        // int marks;
+        // cin>>marks;
 
-        if(marks<24){
-            cout<<"F";
-        }
-        else if( marks < 44){
-            cout<<"E";
-        }
-        else if(marks < 64){
-            cout<<"D";
-        }
-        else if(marks < 74){
-            cout<<"C";
-        }
-        else if(marks < 90){
-            cout<<"B";
-        }
-        else if(marks <= 100){
-            cout<<"A"; 
+        // if(marks<24){
+        //     cout<<"F";
+        // }
+        // else if( marks < 44){
+        //     cout<<"E";
+        // }
+        // else if(marks < 64){
+        //     cout<<"D";
+        // }
+        // else if(marks < 74){
+        //     cout<<"C";
+        // }
+        // else if(marks < 90){
+        //     cout<<"B";
+        // }
+        // else if(marks <= 100){
+        //     cout<<"A"; 
+        // }
+
+        int day;
+        cin>>day;
+
+        switch (day){
+            case 1:
+            cout<<"Monday"<<endl;
+            break;
+            case 2:
+            cout<<"Tuesday"<<endl;
+            break;
+            case 3:
+            cout<<"wednesday"<<endl;
+            break;
+            case 4:
+            cout<<"Thrusday"<<endl;
+            break;
+            case 5:
+            cout<<"Friday"<<endl;
+            break;
+            case 6:
+            cout<<"Saturday"<<endl;
+            break;
+            case 7:
+            cout<<"Sunday"<<endl;
+            break;
+            default:
+            cout<<"There is not number of day"<<endl;
         }
 
 
