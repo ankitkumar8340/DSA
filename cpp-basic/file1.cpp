3839412dc1e382fcf83a9d6d6affccc790059a7b
ankit <ankitkumarswe01@gmail.com>
2026-02-27 16:20:01 +0530

    updated

---
+++ cpp-basic/file1.cpp
@@ -0,0 +1,43 @@
+// #include <iostream>
+#include <bits/stdc++.h>
+// #include <math.h>
+using namespace std;
+
+int main(){
+    // cout<<"Hey ankit"<<endl;
+    int x,y;
+    cin>>x>>y;
+    cout<<x<<" "<<y;
+
+    float x = 4.6;
+    float y = 3;
+
+
+
+
+
+
+
+    
+    return 0;            
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
