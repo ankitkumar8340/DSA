a769cfaddc7f3081c0309fad7d76c8b0c9fc57f6
ankit <ankitkumarswe01@gmail.com>
2026-03-03 16:58:14 +0530

    updated

--- pattern/file1.cpp
+++ pattern/file1.cpp
@@ -213,8 +213,88 @@ void print9(int n){
 }
 
 void print10(int n){
+    /*
+        n= 5
+        outer = 2*n-1
+        stars = i
+        if(i > n ) stars = 2*n-i
+    1   *
+    2   **
+    3   ***
+    4   ****
+    5   *****
+    6   ****
+    7   ***
+    8   **
+    9   *
+    */
+
+    for(int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i > n ) stars = 2*n -i;
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
 
 }
+
+void print11(int n){
+    /*
+
+    1
+    0 1
+    1 0 1
+    0 1 0 1
+    1 0 1 0 1
+    
+    */
+   for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        if(i%2==0){
+            if(j%2 != 0){
+                cout<<"0 ";
+            }else{
+            cout<<"1 ";
+            }
+        }
+        else if(i%2!=0){
+            if(j%2 != 0){
+                cout<<"1 ";
+            }else{
+            cout<<"0 ";
+            }
+        }
+
+    }
+    cout<<endl;
+   }
+
+//    or
+
+for(int i=0;i<n;i++){
+    int start=1;
+    if(i%2==0) start=1;
+    else start=0;
+    for(int j=0;j<i;j++){
+        cout<<start;
+        start=1-start;   //its flip the value 1-1 =0  and 1-0 = 1
+    }
+    cout<<endl;
+}
+
+
+
+}
+
+
+
+
+
+
+
 int main(){
 
    int n;
@@ -228,7 +308,9 @@ int main(){
     // print7(n);
     // print8(n);
     // print9(n);
-    print10(n);
+    // print10(n);
+    print11(n);
+
    
 
 
