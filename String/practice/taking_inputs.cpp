4346d6f8e7d0bc033bad9b8ed9b64b250d49ed9f
Ankit Kumar <ankitkumarswe01@gmail.com>
2026-07-07 18:09:55 +0530

    taking inputs in string for a single word and a for a whole sentence

---
+++ String/practice/taking_inputs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    string sentence, word;
+
+    cin>>n;
+    cin.ignore();
+    getline(cin, sentence);
+    // cin.ignore();
+    cin>>word;
+
+    cout<<n<<endl;
+    cout<<sentence<<endl;
+    cout<<word<<endl;
+
+
+}
+
+
+
+
+
+
+
+
